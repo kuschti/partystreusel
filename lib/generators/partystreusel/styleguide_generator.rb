8210b45d0ced79cb034d4f02238f4b7a9261441e
Felipe Kaufmann <felipekaufmann@gmail.com>
2015-04-20 13:45:55 +0200

    add missing steps to generator

--- lib/generators/partystreusel/styleguide_generator.rb
+++ lib/generators/partystreusel/styleguide_generator.rb
@@ -16,5 +16,15 @@ module Partystreusel
       directory 'styleguide/source/styleguide', TARGET_DIR_FOR_STYLEGUIDE
       directory 'styleguide/source/partials', TARGET_DIR_FOR_PARTIALS
     end
+
+    def add_route
+      inject_into_file 'config/routes.rb', 'mount Partystreusel::Engine => "/"', after: /^end/
+    end
+
+    def copy_grunticon_files
+      directory 'app/assets/images', 'app/assets/images'
+      copy_file 'package.json', 'package.json'
+      copy_file 'Gruntfile.coffee', 'Gruntfile.coffee'
+    end
   end
 end
