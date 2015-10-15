63bc456ad34615abf6bdba4f7c68a0db890a773e
kuschti <kuschti@gmail.com>
2015-10-15 15:20:44 +0200

    add gulp-imagemmin

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,10 @@
 var gulp = require('gulp'),
     del = require('del'),
+    notify = require('gulp-notify'),
     sass = require('gulp-sass'),
     bourbon = require('node-bourbon').includePaths,
-    neat = require('node-neat').includePaths;
+    neat = require('node-neat').includePaths,
+    imagemin = require('gulp-imagemin');
 
 var paths = {
   coffee: './source/javascripts/**/*.coffee',
@@ -10,6 +12,8 @@ var paths = {
   images: './source/images/*'
 }
 
+// STYLES
+// ----------------------------------------
 gulp.task('css', function () {
   gulp.src(paths.sass)
     .pipe(sass({
@@ -18,18 +22,34 @@ gulp.task('css', function () {
     .pipe(gulp.dest('./dist/css'));
 });
 
+// IMAGES
+// ----------------------------------------
+gulp.task('imagemin', function () {
+  gulp.src(paths.images)
+    .pipe(imagemin({
+      progressive: true,
+      svgoPlugins: [{removeViewBox: false}]
+    }))
+    .pipe(gulp.dest('dist/images'));
+});
+
+// -------
 gulp.task('watch', function () {
   // watch .sass files
   gulp.watch(paths.sass, ['css']);
 });
 
+// Clean all dist files
+// ----------------------------------------
 gulp.task('clean', function () {
   return del([
-    'dist/css/**/*'
+    'dist/css/**/*',
+    'dist/js/**/*'
   ]);
 });
 
-//---------------------------------------
+// Default & build tasks
+// ----------------------------------------
 gulp.task('default', ['clean', 'build'], function() {
   gulp.start('watch');
 });
