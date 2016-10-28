ef27ad6500e22e3077337cc7476c4dee43ca0ea3
Phil Schilter <phil.schilter@gmail.com>
2016-10-28 15:52:32 +0200

    use spaces instead of tabs

--- webpack.config.js
+++ webpack.config.js
@@ -3,36 +3,35 @@ var webpack = require('webpack');
 
 module.exports = function(fabricatorConfig) {
 
-	"use strict";
+  "use strict";
 
-	var config = {
-		entry: {
-			'partystreusel/scripts/p': fabricatorConfig.src.scripts.fabricator
-		},
-		output: {
-			path: path.resolve(__dirname, fabricatorConfig.dest, 'assets'),
-			filename: '[name].js'
-		},
-		module: {
-			loaders: [
-				{
-					test: /\.js$/,
-					exclude: /(node_modules|prism\.js)/,
-					loaders: ['babel'],
-					presets: ['es2015', 'stage-2']
-				}
-			]
-		},
-		plugins: [],
-		cache: {}
-	};
+  var config = {
+    entry: {
+      'partystreusel/scripts/p': fabricatorConfig.src.scripts.fabricator
+    },
+    output: {
+      path: path.resolve(__dirname, fabricatorConfig.dest, 'assets'),
+      filename: '[name].js'
+    },
+    module: {
+      loaders: [
+        {
+          test: /\.js$/,
+          exclude: /(node_modules|prism\.js)/,
+          loaders: ['babel'],
+          presets: ['es2015', 'stage-2']
+        }
+      ]
+    },
+    plugins: [],
+    cache: {}
+  };
 
-	if (!fabricatorConfig.dev) {
-		config.plugins.push(
-			new webpack.optimize.UglifyJsPlugin()
-		);
-	}
+  if (!fabricatorConfig.dev) {
+    config.plugins.push(
+      new webpack.optimize.UglifyJsPlugin()
+    );
+  }
 
-	return config;
-
+  return config;
 };
