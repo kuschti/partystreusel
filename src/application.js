84f9aa487bddb2cec3432997ad307564e7b03aa9
Phil Schilter <phil.schilter@gmail.com>
2016-10-29 11:34:10 +0200

    add accordion component

--- src/application.js
+++ src/application.js
@@ -1,9 +1,11 @@
 import 'babel-polyfill';
 import $ from 'jquery';
 import Topbar from './materials/molecules/topbar/topbar';
+import Accordion from './materials/molecules/accordion/accordion';
 
 $(() => {
   $('html').removeClass('no-js');
 
   Topbar.init();
+  Accordion.init();
 });
