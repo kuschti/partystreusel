ca83c535c59a595e958caf49a939c186ecb0eed6
Phil Schilter <phil.schilter@gmail.com>
2016-11-04 22:37:40 +0100

    add dropdown component

--- src/application.js
+++ src/application.js
@@ -3,11 +3,13 @@ import $ from 'jquery';
 import Topbar from './materials/molecules/topbar/topbar';
 import Accordion from './materials/molecules/accordion/accordion';
 import Tab from './materials/molecules/tab/tab';
+import Dropdown from './materials/molecules/dropdown/dropdown';
 
 $(() => {
   $('html').removeClass('no-js');
 
-  Topbar.init();
-  Accordion.init();
+  Topbar.init()
+  Accordion.init()
   Tab.init()
+  Dropdown.init()
 });
