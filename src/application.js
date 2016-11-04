3b671bb09637d5e60aa83df44d9946401a699c66
Phil Schilter <phil.schilter@gmail.com>
2016-11-04 22:27:05 +0100

    add Tab component

--- src/application.js
+++ src/application.js
@@ -2,10 +2,12 @@ import 'babel-polyfill';
 import $ from 'jquery';
 import Topbar from './materials/molecules/topbar/topbar';
 import Accordion from './materials/molecules/accordion/accordion';
+import Tab from './materials/molecules/tab/tab';
 
 $(() => {
   $('html').removeClass('no-js');
 
   Topbar.init();
   Accordion.init();
+  Tab.init()
 });
