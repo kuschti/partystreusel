773cdac9cdca44ccbcad38e32f24daafccb7306c
kuschti <kuschti@gmail.com>
2018-01-11 16:56:36 +0100

    Updates

---
+++ components/raw/accordion/accordion.js
@@ -0,0 +1,43 @@
+import MoveTo from 'moveto';
+import Base from '../../_config/base';
+
+class Accordion extends Base {
+  constructor(el) {
+    super(el);
+
+    this.config = {
+      openClass: 'accordion--open',
+      titleClass: 'accordion__title',
+      itemSelector: '.accordion__item',
+    };
+
+    this.items = this.$el.querySelectorAll(this.config.itemSelector);
+    this.offset = this.$el.getAttribute('data-mt-tolerance');
+
+    this.$el.addEventListener('click', this.toggleItem.bind(this));
+  }
+
+  toggleItem(e) {
+    if (e.target.classList.contains(this.config.titleClass)) {
+      const item = e.target.closest(this.config.itemSelector);
+      const currentOpen = item.classList.contains(this.config.openClass);
+
+      for (let i = 0; i < this.items.length; i += 1) {
+        this.items[i].classList.remove(this.config.openClass);
+      }
+
+      if (!currentOpen) {
+        item.classList.toggle(this.config.openClass);
+      }
+
+      e.preventDefault();
+      const moveTo = new MoveTo({
+        tolerance: this.offset ? this.offset : 0,
+      });
+      moveTo.move(item);
+    }
+  }
+}
+
+Accordion.className = 'Accordion';
+export default Accordion;
---
+++ components/raw/offcanvas/offcanvas.js
@@ -0,0 +1,29 @@
+import Base from '../../_config/base';
+
+class Offcanvas extends Base {
+  constructor(el) {
+    super(el);
+
+    this.config = {
+      openClass: 'offcanvas--open',
+      overlaySelector: '.offcanvas__overlay',
+      openButtonSelector: '.offcanvas__opener',
+      closeButtonSelector: '.offcanvas__close',
+    };
+
+    const togglerOpen = this.$el.querySelector(this.config.openButtonSelector);
+    const togglerClose = this.$el.querySelector(this.config.closeButtonSelector);
+    const overlay = this.$el.querySelector(this.config.overlaySelector);
+
+    togglerOpen.addEventListener('click', this.toggle.bind(this));
+    togglerClose.addEventListener('click', this.toggle.bind(this));
+    overlay.addEventListener('click', this.toggle.bind(this));
+  }
+
+  toggle() {
+    this.$el.classList.toggle(this.config.openClass);
+  }
+}
+
+Offcanvas.className = 'Offcanvas';
+export default Offcanvas;
